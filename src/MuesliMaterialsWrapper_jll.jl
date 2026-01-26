eb18090ab5198b700fb6015178a64acb839c67a0
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2026-01-26 13:08:53 +0100

    MuesliMaterialsWrapper_jll build 0.14.0+0

--- src/MuesliMaterialsWrapper_jll.jl
+++ src/MuesliMaterialsWrapper_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("MuesliMaterialsWrapper")
-JLLWrappers.@generate_main_file("MuesliMaterialsWrapper", UUID("6dead231-e870-550e-b74b-2a3bf2cfaa53"))
+JLLWrappers.@generate_main_file("MuesliMaterialsWrapper", Base.UUID("6dead231-e870-550e-b74b-2a3bf2cfaa53"))
 end  # module MuesliMaterialsWrapper_jll
